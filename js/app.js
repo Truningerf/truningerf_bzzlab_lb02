8872e49b31ef5b72ba4738223317c6c2ecf65c41
Fabian Truninger <truningerf@bzz.ch>
2022-01-13 09:45:09 +0100

    better data structure pagination added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,106 @@
-//picture
-let htmlObj = document.getElementById("picture");
-htmlObj.innerHTML =
-    `<a class="" href="" data-bs-toggle="modal" data-bs-target="${blog1.picture.databstarget}"><img src="../images/${blog1.picture.dataname}" class="card-img-top img-scale" alt="${blog1.picture.alttext}"></a>`;
+//Webapp mit Pagination
 
-//title
-htmlObj = document.getElementById("title");
-htmlObj.innerHTML =
-    `${blog1.text.title}`;
+//Deklaration der Variablen
+let index;
+let blog = undefined;
 
-//description
-htmlObj = document.getElementById("description");
-htmlObj.innerHTML =
-    `${blog1.text.description}`;
+//Ausgabe zeigen
+function showUI() {
+    //picture
+    let htmlObj = document.getElementById("picture");
+    htmlObj.innerHTML =
+        `<a class="" href="" data-bs-toggle="modal" data-bs-target="${blog.picture.databstarget}"><img src="../images/${blog.picture.dataname}" class="card-img-top img-scale" alt="${blog.picture.alttext}"></a>`;
 
-//test if download is possible
-let download = "<em>No Downloads available</em>"
-if (blog1.download.possible){
-    download = `<a href="../downloads/${blog1.download.pdfname}" download="infochart_milk.pdf"><i class="far fa-file-pdf"></i>&nbsp;Download (PDF)</a>`;
+    //title
+    htmlObj = document.getElementById("title");
+    htmlObj.innerHTML =
+        `${blog.text.title}`;
+
+    //description
+    htmlObj = document.getElementById("description");
+    htmlObj.innerHTML =
+        `${blog.text.description}`;
+
+    //test if download is possible
+    let download = "<em>No Downloads available</em>"
+    if (blog.download.possible){
+        download = `<a href="../downloads/${blog.download.pdfname}" download="${blog.download.pdfname}"><i class="far fa-file-pdf"></i>&nbsp;Download (PDF)</a>`;
+    }
+    //download
+    htmlObj = document.getElementById("download");
+    htmlObj.innerHTML =
+        `${download}`;
+
+    //date
+    htmlObj = document.getElementById("date");
+    htmlObj.innerHTML =
+        `${blog.date}`;
+
+    //Index im Pagination-Element anzeigen
+    htmlObj = document.getElementById("showIndex")
+    //set
+    htmlObj.innerHTML = index + 1;
 }
-//download
-htmlObj = document.getElementById("download");
-htmlObj.innerHTML =
-    `${download}`;
 
-//date
-htmlObj = document.getElementById("date");
-htmlObj.innerHTML =
-    `${blog1.date}`;
+//Vorheriger Eintrag zeigen
+function showPrevious() {
+    //Index aus dem Browser-Speicher lesen
+        index = localStorage.getItem("index");
+        //Testausgabe auf der Console
+        console.log(index);
+        //Falls der index nicht definiert ist ...
+        if (index === undefined){
+            //... dann setze den index auf 0 (Anfang)
+            index = 0;
+        } else {
+            //... sonst falls der index eins grösser als das Minimum ist
+            if (index > 0){
+                //... erhöhe den index um 1
+                index--;
+            }
+        }
+        //speichere wieder den index im Browser-Speicher
+        localStorage.setItem("index", index);
+        //hole den nächst höheren Eintrag
+        blog = blogliste[index];
+        //zeige den Eintrag
+        showUI();
+}
+
+//Nächster Eintrag zeigen
+function showNext() {
+    //Index aus dem Browser-Speicher lesen
+    index = localStorage.getItem("index");
+    //Testausgabe auf der Console
+    console.log(index);
+    //Falls der index nicht definiert ist ...
+    if (index === undefined){
+        //... dann setze den index auf 0 (Anfang)
+        index = 0;
+    } else {
+        //... sonst falls der index eins kleiner als das Maximum ist
+        if (blogliste.length-1 > index){
+            //... erhöhe den index um 1
+            index++;
+        }
+    }
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    blog = blogliste[index];
+    //zeige den Eintrag
+    showUI();
+}
+
+//start app
+//Falls der index nicht definiert ist ...
+if (blog === undefined){
+    //... dann setze den index auf 0 (Anfang)
+    index = 0;
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    blog = blogliste[index];
+    //zeige den Eintrag
+    showUI();
+}
